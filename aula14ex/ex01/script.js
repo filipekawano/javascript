348ad55f59e3c4f88dd8776d22ef0eb1476aa050
Filipe Kawano <164102164+filipekawano@users.noreply.github.com>
2024-05-31 18:51:05 -0300

    exercicio de repaticao finalizados

--- aula14ex/ex01/script.js
+++ aula14ex/ex01/script.js
@@ -3,19 +3,30 @@ var botao = document.querySelector('div#botao')
 botao.addEventListener('click', showResult)
 
 function showResult() {
-    var inicio = Number(document.querySelector('input#inicio').value)
-    var fim = Number(document.querySelector('input#fim').value)
-    var passo = Number(document.querySelector('input#passo').value)
-    if (inicio === '' || fim == '') {
+    var inicio = document.querySelector('input#inicio').value
+    var fim = document.querySelector('input#fim').value
+    var passo = document.querySelector('input#passo').value
+    if (inicio == '' || fim == '' || passo == '') {
         resultado.innerHTML = 'Impossível contar!'
     } else {
-        if (passo === 0) {
+        inicio = Number(inicio)
+        fim = Number(fim)
+        passo = Number(passo)
+        if (passo <= 0) {
             alert('Passo inválido! Considerando PASSO 1')
             passo = 1
         }
         resultado.innerHTML = `<p>Contando:</p>`
-        for (var c = inicio; c <= fim; c += passo) {
-            resultado.innerHTML += `${c} &#x1F449; `
+        if (inicio > fim) {
+            // Contagem regressiva
+            for (var c = inicio; c >= fim; c -= passo) {
+                resultado.innerHTML += `${c} &#x1F449; `
+            }    
+        } else {
+            // Contagem crescente
+            for (var c = inicio; c <= fim; c += passo) {
+                resultado.innerHTML += `${c} &#x1F449; `
+            }    
         }
         resultado.innerHTML += '&#x1F6A9;'    
     }
--- aula14ex/ex02/script.js
+++ aula14ex/ex02/script.js
@@ -2,13 +2,18 @@ var botao = document.querySelector('div#botao')
 botao.addEventListener('click', gerarTabuada)
 
 function gerarTabuada() {
-    var numero = Number(document.querySelector('input#numero').value)
-    var result = document.querySelector('select#result')
-    for (var c = 1; c <= 10; c++) {
-        if (c == 1) {
-            result.innerHTML = `<option value="tabuada">${numero} x ${c} = ${numero * c}</option>`    
-        } else {
-            result.innerHTML += `<option value="tabuada">${numero} x ${c} = ${numero * c}</option>`
+    let numero = document.querySelector('input#numero').value
+    let result = document.querySelector('select#result')
+    if (numero.length == 0) {
+        alert('Por favor, digite algum número!')
+    } else {
+        numero = Number(numero)
+        for (let c = 1; c <= 10; c++) {
+            if (c == 1) {
+                result.innerHTML = `<option value="v${c}">${numero} x ${c} = ${numero * c}</option>`    
+            } else {
+                result.innerHTML += `<option value="v${c}">${numero} x ${c} = ${numero * c}</option>`
+            }
         }
     }
 }
