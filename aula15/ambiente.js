5bce0f28ab6166c4f0451bb89810fcf5ceb1a527
Filipe Kawano <164102164+filipekawano@users.noreply.github.com>
2024-05-31 21:04:09 -0300

    aula 15 criada

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 2, 9]
+num[1] = 0
+console.log(num)
+
+num.push(4)
+console.log(num)
+
+num.sort() // Ordenar vetor
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O segundo valor do vetor é ${num[1]}`)
